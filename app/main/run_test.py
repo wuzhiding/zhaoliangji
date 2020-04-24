a93299ab21ddb8ec92fbe896fe0e833fffdd9b03
wuzhiding <1005706322@qq.com>
2020-04-24 15:05:11 +0800

    2

--- app/main/run_test.py
+++ app/main/run_test.py
@@ -10,13 +10,15 @@ class RunTest:
     # 程序执行的主入口
     def go_on_run(self):
         rows_count = self.data.get_case_lines()
+
         list=[]
         for i in range(1, rows_count):
-            url = self.data.get_url(i)
-            data = self.data.get_data_for_json(i)
-            request_way = self.data.get_request_way(i)
             run = self.data.get_run(i)
-            token = self.data.is_token(i)
+            if run:
+                url = self.data.get_url(i)
+                data = self.data.get_data_for_json(i)
+                request_way = self.data.get_request_way(i)
+                token = self.data.is_token(i)
             if run:
                 res = self.run_method.run_main(request_way, url, data, token)
                 list.append(res)
